16c3009fead3cf85945b2f239d0eb177cb446ceb
dayanne-castro <dayanne@DAYANNEMDC.BIO.NYU.EDU>
2017-04-05 10:52:55 -0400

    add TCGA network, and decrypt files ipynb

---
+++ TCGA/combine_output_networks.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+network1 = pd.read_csv('TCGA_BBSR1.1_ChromPrior_TfsPrior.tsv', sep = '\t')
+network2 = pd.read_csv('TCGA_BBSR1.1_TrrustPrior_TfsPrior.tsv', sep = '\t')
+
+# TRRUST prior -- grouped predictors
+# pred.group.1 -> 'RFXANK' 'RFXAP'
+grouped_predictors = network2.loc[network2['regulator'] == 'pred.group.1',:]
+ungrouped = []
+for interaction, row in grouped_predictors.iterrows():
+    tmp_row = row.copy()
+    tmp_row['regulator'] = 'RFXANK'
+    ungrouped.append(tmp_row)
+    tmp_row = row.copy()
+    tmp_row['regulator'] = 'RFXAP'
+    ungrouped.append(tmp_row)
+ungrouped = pd.DataFrame(ungrouped)
+network2 = pd.concat([network2, ungrouped])
+network2 = network2.loc[~(network2['regulator'] == 'pred.group.1'),:]
+
+network1['prior'] = 'CHROM'
+network2['prior'] = 'TRRUST'
+
+network = pd.concat([network1, network2])
+network_final = []
+
+for interaction, df in network.groupby(['regulator', 'target']):
+    df = df.loc[:,['regulator', 'target', 'beta.sign.sum', 'prior']]
+    if df.shape[0] > 1:
+        value = np.sign(df['beta.sign.sum'][df['prior'] == 'TRRUST'])*(np.abs(df['beta.sign.sum'].tolist()).max())
+        df['beta.sign.sum'] = value
+        df['prior'] = 'BOTH'
+        df = df.dropna()
+    network_final.append(df)
+
+network_final = pd.concat(network_final)
+network_final = network_final.iloc[:,[0,1,2]]
+network_final.columns = ['regulator', 'target', 'bootstraps']
+network_final.loc[:,'bootstraps'] = network_final['bootstraps'].astype(str)
+
+network_final.to_csv('TCGA_BBSR1.1_combinedPriors_TfsPriors.tsv', sep = '\t', index = False)
